ffada6d70c3c01f353f68fe8e946459a4aef43b9
20250419ehe <1198578873@qq.com>
2026-06-08 18:43:30 +0800

    feat: 配置Navigation导航

--- app/src/main/java/com/example/notemaster/MainActivity.java
+++ app/src/main/java/com/example/notemaster/MainActivity.java
@@ -7,18 +7,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        
+        // 设置窗口边距
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        
+        // 初始化Navigation
+        setupNavigation();
+    }
+    
+    private void setupNavigation() {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        
+        if (navHostFragment != null) {
+            navController = navHostFragment.getNavController();
+            // 如果有底部导航栏，可以在这里设置
+            // NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        }
+    }
+    
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp() || super.onSupportNavigateUp();
     }
 }
